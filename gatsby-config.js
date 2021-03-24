df6fc2819f4b70c67e0914ba672e9c46d749fc4b
birudeghi <birudghi@gmail.com>
2021-03-24 21:24:13 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: "edias-space",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-datocms",
+      options: {
+        apiToken: "29a318bb029f15e51cea92b82d120a",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-89529803-1",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
