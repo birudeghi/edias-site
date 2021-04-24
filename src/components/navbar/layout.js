1494c232b38396dc61c83da53079c05ba18fb2cb
birudeghi <birudghi@gmail.com>
2021-04-24 17:32:23 +0100

    added Privacy Policy and Terms

--- src/components/navbar/layout.js
+++ src/components/navbar/layout.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { StaticQuery, graphql } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import Header from "./header";
 import { Helmet } from 'react-helmet';
+import './_layout.scss';
+
+import fb from "../../images/fb.png";
+import twitter from "../../images/twitter.png";
+import insta from "../../images/insta.png";
+import linkedin from "../../images/linkedin.png";
 
 const Layout = ({ children }) => (
     <StaticQuery
@@ -43,6 +49,28 @@ const Layout = ({ children }) => (
           >
             {children}
           </div>
+          <div className="footer-section">
+            <div className="social-media">
+                <a href="https://www.facebook.com/heycuvo" target="_blank" rel="noopener noreferrer">
+                    <img src={fb} className="social-media--img" alt="Responsive" />  
+                </a>
+                <img src={twitter} className="social-media--img" alt="Responsive" />
+                <img src={insta} className="social-media--img" alt="Responsive" />
+                <img src={linkedin} className="social-media--img" alt="Responsive" />
+            </div>
+            <div className="legal">
+                <li className="link-privacy">
+                    <Link className="link-privacy-text" to="/privacy">
+                        Privacy
+                    </Link>
+                </li>
+                <li className="link-terms">
+                    <Link className="link-terms-text" to="/terms">
+                        Terms
+                    </Link>
+                </li>
+            </div>
+        </div>
         </React.Fragment>
       )}
     />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import * as React from "react"
 import AddWaitlist from '../components/AddWaitlist';
-import { Link } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
 import Layout from "../components/navbar/layout";
 import "./_index.scss";
 import hero from "../images/edias-hero.png";
-import fb from "../images/fb.png";
-import twitter from "../images/twitter.png";
-import insta from "../images/insta.png";
-import linkedin from "../images/linkedin.png";
 
 const LandingPage = () => {
 
@@ -31,16 +26,6 @@ const LandingPage = () => {
                 </div>
                 <img src={hero} className="hero-img" alt="Responsive" />
                 <AddWaitlist className="hero-input" />
-            </div>
-        </div>
-        <div className="footer-section">
-            <div className="social-media">
-                <a href="https://www.facebook.com/heycuvo" target="_blank" rel="noopener noreferrer">
-                    <img src={fb} className="social-media--img" alt="Responsive" />  
-                </a>
-                <img src={twitter} className="social-media--img" alt="Responsive" />
-                <img src={insta} className="social-media--img" alt="Responsive" />
-                <img src={linkedin} className="social-media--img" alt="Responsive" />
             </div>
         </div>
     </Layout>
