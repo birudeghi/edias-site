700896ea2aa0e1eb328331783e878b24e3c18bd7
birudeghi <birudghi@gmail.com>
2021-07-05 00:18:06 +0100

    added code

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,34 @@ import { Helmet } from 'react-helmet';
 import SectionContainer from "../components/SectionContainer";
 import SectionStep from "../components/SectionStep";
 import Layout from "../components/navbar/layout";
+import Code from "../images/python.svg";
 import "./_index.scss";
 
 const LandingPage = () => {
 
     const STEPS = [
         {
-            icon: <i className="bi bi-phone-fill" style={{"font-size": "4rem"}}></i>,
-            text: "Describe your mission in a 5-minute form or phone call."    
+            icon: <i className="bi bi-code-square" style={{"font-size": "4rem"}}></i>,
+            text: "Develop immediately from day one."    
         },
         {
             icon: <i className="bi bi-person-fill" style={{"font-size": "4rem"}}></i>,
-            text: "We will match you to an expert who will be your dedicated engineer."    
+            text: "Request new libraries and functions through a 5-minute form or phone call."    
         },
         {
             icon: <i className="bi bi-cup-fill" style={{"font-size": "4rem"}}></i>,
-            text: "We'll take care of your avionics and flight software, and you'll focus on your mission and payload."    
+            text: "Complexity managed. Now you can focus on your mission and payload."    
+        }
+    ];
+
+    const TECH = [
+        {
+            icon: <i className="fab fa-python python" style={{"font-size": "4rem"}}></i>,
+            text: "Develop easily with PyCubed, the simplest satellite stack built on Python."
+        },
+        {
+            icon: <i className="bi bi-intersect" style={{"font-size": "4rem"}}></i>,
+            text: "Exploit the prebuilt integrations to focus more on your core mission capabilities."
         }
     ];
 
@@ -61,15 +73,31 @@ const LandingPage = () => {
                 <div>
                 </div>
             </SectionContainer>
+            <SectionContainer className="section-technical">
+                <div className="technical-text col-sm-6">
+                    <h2 className="technical-title display-4">
+                        Made by developers, for developers.
+                    </h2>
+                    <h4 className="technical-content--text">
+                        We agonise over the right technology so that you don't have to spend months reading manuals and stitching together disparate subsystems.
+                    </h4>
+                    <div className="technical-content--details">
+                        <SectionStep contents={TECH} />
+                    </div>
+                </div>
+                <div className="technical-code col-sm-6">
+                    <img className="code-img" src={Code} alt="code"></img>
+                </div>
+            </SectionContainer>
         </div>
         <SectionContainer className="section-price" backgroundColor="dark">
             <div className="row flex-row price-text">
                 <div className="price-title-container col">
                     <h2 className="price-title display-4">
-                        Talk to us for free
+                        It costs as little as your phone.
                     </h2>
                     <h4 className="price-content--text">
-                        We are currently testing this model, so everything we do won't cost you anything. We promise.
+                        All included, like your phone.
                     </h4>
                     <AddWaitlist className="hero-input" />
                 </div>
